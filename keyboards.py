39ce4585f4e14d1490b040e3761b28a57d0375fb
fattakhova003-wq <fattakhova003@gmail.com>
2026-07-11 20:04:35 +0300

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,28 @@
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+
+
+keyboard_start = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "✅ ПОДТВЕРДИТЬ ПОЛУЧЕНИЕ",
+                callback_data="confirm"
+            )
+        ]
+    ]
+)
+
+
+keyboard_wait = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "🛰 ОЖИДАЮ ДАЛЬНЕЙШИХ ИНСТРУКЦИЙ",
+                callback_data="wait"
+            )
+        ]
+    ]
+)
