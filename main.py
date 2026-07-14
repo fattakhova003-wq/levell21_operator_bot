bc1f5da8bd6a4dac417e52a7e948af0b81c6b6a9
fattakhova003-wq <fattakhova003@gmail.com>
2026-07-14 14:32:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,6 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await query.message.reply_text(
             MESSAGES["location"]
         )
-    )
 
 
 def main():
