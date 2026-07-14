d9ba21477fc06310c193fcbe6165eacc1981e06e
fattakhova003-wq <fattakhova003@gmail.com>
2026-07-14 16:39:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     query = update.callback_query
-
     await query.answer()
 
 
@@ -142,5 +141,4 @@ def main():
 
 
 if __name__ == "__main__":
-
     main()
