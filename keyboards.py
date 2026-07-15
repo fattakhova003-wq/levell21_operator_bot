57b02f2c23542f83ef1d6721fdb5312d032d0453
fattakhova003-wq <fattakhova003@gmail.com>
2026-07-15 13:57:32 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,3 @@
-```python
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -6,27 +5,32 @@ keyboard_start = InlineKeyboardMarkup(
     [[InlineKeyboardButton("✅ ПОДТВЕРДИТЬ ПОЛУЧЕНИЕ", callback_data="confirm")]]
 )
 
+
 keyboard_wait = InlineKeyboardMarkup(
     [[InlineKeyboardButton("🛰 ОЖИДАЮ ДАЛЬНЕЙШИХ ИНСТРУКЦИЙ", callback_data="wait")]]
 )
+
 
 keyboard_hotel = InlineKeyboardMarkup(
     [[InlineKeyboardButton("🏨 ПОДТВЕРДИТЬ ОТЕЛЬ", callback_data="hotel")]]
 )
 
+
 keyboard_tube = InlineKeyboardMarkup(
     [[InlineKeyboardButton("🌪 ЗАПУСТИТЬ ИСПЫТАНИЕ", callback_data="tube")]]
 )
 
+
 keyboard_football = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("⚽ ПРИНЯТЬ ВЫЗОВ", callback_data="
-```
-```python
+    [[InlineKeyboardButton("⚽ ПРИНЯТЬ ВЫЗОВ", callback_data="football")]]
+)
+
+
 keyboard_lounge = InlineKeyboardMarkup(
     [[InlineKeyboardButton("🛋 ОТКРЫТЬ LOUNGE", callback_data="lounge")]]
 )
 
+
 keyboard_midnight = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("🌙 НАЧАТЬ НОЧНОЙ ПРОТОКОЛ", callback_data="midnight")]]
+    [[InlineKeyboardButton("🌙 НАЧАТЬ НОЧНОЙ ЭТАП", callback_data="midnight")]]
 )
-```
